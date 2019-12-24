444068fc64b83490f4145e02f8ae0105e8ed6bd1
Ken Obata <48650395+KenObata@users.noreply.github.com>
2019-12-24 18:38:28 +0900

    Add files via upload

---
+++ function_gram_schmidt.py
@@ -0,0 +1,59 @@
+# Author: Ken Obata
+# Created Date: December, 24, 2019
+# Description:
+# This program is called from from other .py file as a single function.
+# Function returns result of gram-schmidt process.
+# At the end, user gets orthonomal basis.
+
+
+import sys
+import numpy
+import math
+import scipy.linalg as la
+from numpy.linalg import matrix_power
+
+
+# Function proj:
+# Project vector v onto w.
+def proj(v,w):
+    v = numpy.array(v)
+    w = numpy.array(w)
+    scalar = float(numpy.sum(v * w)) / float(numpy.sum(w * w))
+    
+    return  numpy.array([scalar * element for element in w])
+
+# Function gram_schmidt:
+# Return orthonormal basis where it is a set of vector {v1,v2,...vn}
+def gram_schmidt(Matrix):
+    
+    # variable declaration
+    orthogonal_basis = []
+    ortho_normal_basis = []
+    size = len(Matrix)
+    
+    # Base vector
+    v1 = Matrix[:,0]
+    orthogonal_basis.append(v1)
+    
+    # If, Matrix has only one column, then return the orthogonal_basis.
+    if(len(Matrix) == 1):
+        return orthogonal_basis
+
+    for i in range(1, len(Matrix)):
+        # First create first projection.
+        sum_proj = proj(Matrix[:,i] ,orthogonal_basis[0])
+
+        # Perp Vi = Vi - (proj(Vi, v1) + proj(Vi, v2) + ...) <- get this sum of proj.
+        for j in range(1, len(orthogonal_basis)):
+            sum_proj += proj(Matrix[:,i] ,orthogonal_basis[j])
+       
+        orthogonal_basis.append(Matrix[:,i] - sum_proj)
+    
+    #normalize vectors
+    for i in range(len(orthogonal_basis)):
+        temp = orthogonal_basis[i] \
+        /math.sqrt(numpy.sum(orthogonal_basis[i] * orthogonal_basis[i]))
+        
+        ortho_normal_basis.append(temp)
+
+    return ortho_normal_basis
---
+++ orthogonally_diagonalize.py
@@ -0,0 +1,69 @@
+# Author: Ken Obata
+# Created Date: December, 24, 2019
+# Description:
+# This program reads input text file line by line and
+# returns result of orthogonal diagonalization of a symmetric matrix.
+# The equation is: input matrix = P*D*P_transpose
+# where D is diagonal matrix with eigenvalues on diagonal entries.
+# P is list of corresponding eigen vectors.
+
+
+import sys
+import numpy
+import scipy.linalg as la
+from numpy.linalg import matrix_power
+import function_gram_schmidt as gram
+
+
+def diagonal_matrix(Matrix):
+    eigen_value, eigen_vector=numpy.linalg.eig(Matrix)
+    
+    return numpy.diag(tuple(eigen_value))
+
+def eigenvector(Matrix):
+    eigen_value, eigen_vector=numpy.linalg.eig(Matrix)
+    
+    return eigen_vector
+
+# Function isSymmetrix(Matrix):
+# Given a Matrix, this function returns True if Matrix is Symmetric.
+# Return False otherwise.
+
+def isSymmetric(Matrix):
+    transpose = Matrix.T
+    # If matrix is not symmetric square, print error message.
+    for i in range(len(Matrix)):
+        for j in range(len(Matrix[i]) ):
+            if(Matrix[i][j] != transpose[i][j]):
+                print("Matrix is not square Matrix.")
+                return False
+    return True
+
+
+#get input
+Matrix=[]
+for line in sys.stdin:
+    Array=line.split()
+    Array = [int(i) for i in Array]
+    Matrix.append(Array)
+
+print("Input Matrix is:")
+Matrix = numpy.array(Matrix)
+print(Matrix)
+
+#check if Matrix is Symmetric
+if(isSymmetric(Matrix) is True):
+    print("-----Ortho Diagonalize Input = P * D * P transpose-----")
+    P = eigenvector(Matrix)
+    
+    #check orthogonality of columns in P
+    print("---------Matrix P:-------------")
+    P = numpy.array(gram.gram_schmidt(P)).T
+    print(P)
+    
+    print("---------Matrix D:-------------")
+    D = diagonal_matrix(Matrix)
+    print(D)
+
+    print("------Matrix P trans:----------")
+    print(P.T)
