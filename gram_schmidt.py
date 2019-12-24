e2ca290b265b25fe2e4bf2f0b81851ce0a12ccf2
KenObata <48650395+KenObata@users.noreply.github.com>
2019-12-24 16:28:35 +0900

    Add files via upload

--- gram_schmidt.py
+++ gram_schmidt.py
@@ -18,7 +18,9 @@ from numpy.linalg import matrix_power
 def proj(v,w):
     v = numpy.array(v)
     w = numpy.array(w)
-    return numpy.sum(v * w)/numpy.sum(w * w) * w
+    scalar = float(numpy.sum(v * w)) / float(numpy.sum(w * w))
+    
+    return  numpy.array([scalar * element for element in w])
 
 # Function gram_schmidt:
 # Return orthonormal basis where it is a set of vector {v1,v2,...vn}
@@ -33,13 +35,19 @@ def gram_schmidt(Matrix):
     v1 = Matrix[:,0]
     orthogonal_basis.append(v1)
     
-    # Perp v2 = v2 - Proj(v2, v1) #projection of V2 onto v1
-    v2 = Matrix[:,1]  - proj(Matrix[:,1] ,v1)
-    orthogonal_basis.append(v2)
-    
-    # Perp v3 = v3 - (Proj(v3, v1) + proj(v3, v2))
-    v3 = Matrix[:,2] - proj(Matrix[:,2], v1) - proj(Matrix[:,2], v2)
-    orthogonal_basis.append(v3)
+    # If, Matrix has only one column, then return the orthogonal_basis.
+    if(len(Matrix) == 1):
+        return orthogonal_basis
+
+    for i in range(1, len(Matrix)):
+        # First create first projection.
+        sum_proj = proj(Matrix[:,i] ,orthogonal_basis[0])
+
+        # Perp Vi = Vi - (proj(Vi, v1) + proj(Vi, v2) + ...) <- get this sum of proj.
+        for j in range(1, len(orthogonal_basis)):
+            sum_proj += proj(Matrix[:,i] ,orthogonal_basis[j])
+       
+        orthogonal_basis.append(Matrix[:,i] - sum_proj)
     
     #normalize vectors
     for i in range(len(orthogonal_basis)):
@@ -47,8 +55,7 @@ def gram_schmidt(Matrix):
         /math.sqrt(numpy.sum(orthogonal_basis[i] * orthogonal_basis[i]))
         
         ortho_normal_basis.append(temp)
-    
-    
+
     return ortho_normal_basis
 
 
@@ -63,6 +70,6 @@ for line in sys.stdin:
 print("Input Matrix is:")
 print(numpy.array(Matrix))
 
-print("---------Gram Schmidt Process-------------")
+print("---------Gram Schmidt Process (Orthonormal basis)-------------")
 ortho_normal_basis = gram_schmidt(numpy.array(Matrix))
 print(numpy.array(ortho_normal_basis))
